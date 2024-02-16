0d7aa53167ccefa98675931f8e0e9b3def8ebf63
Suryansh Jain <suryanshjain531@gmail.com>
2024-02-17 00:37:07 +0530

    initial commit

---
+++ src/components/tic-tac-toe.jsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+
+function Square({ value, onClick }) {
+  return (
+    <button onClick={onClick} className="border font-mono float-left text-4xl font-bold h-28 p-0 text-center w-28 -mr-1 -mt-1 cursor-pointer bg-blue-900 text-white">
+      {value}
+    </button>
+  );
+}
+
+export default function TicTacToe() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+  const [isXTurn, setIsXTurn] = useState(true);
+  const [status, setStatus] = useState("");
+
+  function getWinner(squares) {
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+    ];
+
+    for (let i = 0; i < winningPatterns.length; i++) {
+      const [x, y, z] = winningPatterns[i];
+
+      if (
+        squares[x] &&
+        squares[x] === squares[y] &&
+        squares[x] === squares[z]
+      ) {
+        return squares[x];
+      }
+    }
+
+    return null;
+  }
+
+  function handleClick(getCurrentSquare) {
+    let cpySquares = [...squares];
+    if (getWinner(cpySquares) || cpySquares[getCurrentSquare]) return;
+    cpySquares[getCurrentSquare] = isXTurn ? "X" : "O";
+    setIsXTurn(!isXTurn);
+    setSquares(cpySquares);
+  }
+
+  function handleRestart() {
+    setIsXTurn(true);
+    setSquares(Array(9).fill(""));
+  }
+
+  useEffect(() => {
+    if (!getWinner(squares) && squares.every((item) => item !== "")) {
+      setStatus(`This is a draw ! Please restart the game`);
+    } else if (getWinner(squares)) {
+      setStatus(`Winner is ${getWinner(squares)}. Please restart the game`);
+    } else {
+      setStatus(`Next player is - ${isXTurn ? "X" : "O"}`);
+    }
+  }, [squares, isXTurn]);
+
+  console.log(squares);
+
+  return (
+    <div>
+        <div className="font-mono text-5xl font-bold m-24">Tic Tac Toe</div>
+    <div className="flex flex-col items-center mt-40">
+      <div className="flex flex-row">
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
+      </div>
+      <div className="flex flex-row">
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
+      </div>
+      <div className="row">
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
+      </div>
+      <div className="mt-8 text-3xl font-mono">{status}</div>
+      <button className="bg-cyan-600 px-5 py-3 rounded-md text-white text-xl mt-16" onClick={handleRestart}>Restart</button>
+    </div>
+    </div>
+  );
+}
